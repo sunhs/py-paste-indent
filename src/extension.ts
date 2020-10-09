558607975890aa4937526e826365ead73105e358
zhuhaisheng <zhuhaisheng@bigo.sg>
2020-10-09 10:20:50 +0800

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	modified:   CHANGELOG.md#	modified:   README.md#	modified:   package.json#	modified:   src/extension.ts#- Optimized for speed.- Fixed for pasting into a selected region.

--- src/extension.ts
+++ src/extension.ts
@@ -10,69 +10,58 @@ let pasteIndent = () => {
     if (editor === undefined)
         return;
 
-    let baseIndent = editor.selection.active.character,
+    let baseIndent = editor.selection.start.character,
+        insertSpaces = Boolean(editor.options.insertSpaces),
         tabSize = Number(editor.options.tabSize),
-        insertSpaces = Boolean(editor.options.insertSpaces);
+        blank = ' ';
+    if (!insertSpaces)
+        blank = '\t';
 
     vscode.env.clipboard.readText().then((s) => {
         let sep = '\n';
         if (s.indexOf('\r\n') != -1)
             sep = '\r\n';
 
-        let lines = s.split(sep);
-        if (lines.filter((line) => {
-            return line != '';
-        }).length <= 1) {
-        // if (lines[lines.length - 1] == '')
-        //     lines.pop();
-        // if (lines.length <= 1) {
-            vscode.env.clipboard.writeText(s).then(() => {
-                vscode.commands.executeCommand('editor.action.clipboardPasteAction');
-            });
+        let lines = s.split(sep),
+            nonEmptyCnt = 0,
+            firstLineNotEmpty = -1,
+            indentForFirstLineNotEmpty = -1;
+        for (var i = 0; i < lines.length; ++i) {
+            if (lines[i].trim() != '') {
+                ++nonEmptyCnt;
+                if (i != 0 && firstLineNotEmpty == -1 && indentForFirstLineNotEmpty == -1) {
+                    firstLineNotEmpty = i;
+                    indentForFirstLineNotEmpty = lines[i].search(/\S/);
+                }
+            }
+            if (nonEmptyCnt > 1)
+                break;
+        }
+        if (nonEmptyCnt <= 1) {
+            vscode.commands.executeCommand('editor.action.clipboardPasteAction');
             return;
         }
 
         let blockIndent = 0;
         for (var endChar of EndChar) {
-            if (lines[0].endsWith(endChar))
+            if (lines[0].endsWith(endChar)) {
                 if (insertSpaces)
                     blockIndent = tabSize;
                 else
                     blockIndent = 1;
-        }
-
-        let indents = [];
-
-        let firstLineNotEmpty = -1,
-            indentForFirstLineNotEmpty = -1;
-        for (let i = 1; i < lines.length; ++i) {
-            let alphaIndex = lines[i].search(/\S/);
-            if (alphaIndex !== -1) {
-                firstLineNotEmpty = i;
-                indentForFirstLineNotEmpty = alphaIndex;
                 break;
             }
         }
 
-        for (let i = firstLineNotEmpty; i < lines.length; ++i) {
-            let alphaIndex = lines[i].search(/\S/);
-            if (alphaIndex !== -1) {
-                lines[i] = lines[i].substr(alphaIndex);
-                indents.push(alphaIndex - indentForFirstLineNotEmpty);
+        let diff = baseIndent + blockIndent - indentForFirstLineNotEmpty;
+        if (diff != 0) {
+            for (let i = firstLineNotEmpty; i < lines.length; ++i) {
+                if (diff < 0)
+                    lines[i] = lines[i].substr(-diff);
+                else if (diff > 0)
+                    lines[i] = blank.repeat(diff) + lines[i];
             }
-            else
-                indents.push(-1);
         }
-
-        let blank = ' ';
-        if (!insertSpaces)
-            blank = '\t';
-
-        indents.forEach((indent, i) => {
-            let lineIndex = i + firstLineNotEmpty;
-            if (indent !== -1)
-                lines[lineIndex] = blank.repeat(baseIndent + blockIndent + indent) + lines[lineIndex];
-        });
 
         vscode.env.clipboard.writeText(lines.join('\n')).then(() => {
             vscode.commands.executeCommand('editor.action.clipboardPasteAction');
